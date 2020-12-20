0690b9fcfdfa3bd82292868b004ce14133d49296
Tokarev Dmitry <modelist61@bk.ru>
2020-12-21 00:09:45 +0300

    Initial Commit

---
+++ HW3.4 sliderGame int UIKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.4 sliderGame int UIKit
+//
+//  Created by Dmitry Tokarev on 21.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
